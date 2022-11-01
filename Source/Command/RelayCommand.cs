7490509c390c3a1d4e432b5a58780d01d163b926
Leyla Shafiyeva <llshafi03@gmail.com>
2022-11-01 23:50:22 +0400

    Add project files.

---
+++ Source/Command/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Source.Command;
+
+public class RelayCommand : ICommand
+{
+    public event EventHandler? CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+    private readonly Action<object?> _execute;
+    private readonly Predicate<object?>? _canExecute;
+
+    public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
+    {
+        ArgumentNullException.ThrowIfNull(execute, nameof(execute));
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return _canExecute is null || _canExecute.Invoke(parameter);
+    }
+
+    public void Execute(object? parameter)=> _execute.Invoke(parameter);
+    
+}
---
+++ Source/Views/MainView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Source.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Source.Views;
+
+public partial class MainView : Window
+{
+    public ICommand ShowCommand { get; set; }
+    public MainView()
+    {
+        InitializeComponent();
+        DataContext = this;
+        ShowCommand = new RelayCommand(ExecuteShowCommand, CanExecuteShowCommand);
+    }
+    void ExecuteShowCommand(object?parameter)
+    {
+        MessageBox.Show(txtBox.Text);
+    }
+    bool CanExecuteShowCommand(object? parameter)=>txtBox.Text.Length>3;
+    
+}
